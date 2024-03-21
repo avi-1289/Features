7ec1dc97d8844d43597b85694372858f4a950060
avinash-89 <avinashmamidi89@gmail.com>
2024-03-22 04:51:22 +0530

    routes setup

---
+++ ui-features/src/routes/allRoutes.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import ReactApp from "../components/ReactApp/ReactApp";
+import NotFound from "../components/NotFound/NotFound";
+
+const AllRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/react" element={<ReactApp />} />
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+
+export default AllRoutes;
