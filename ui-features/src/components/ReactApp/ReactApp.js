797c936a01b358320f693fb8a57bba0d7cd9634a
avinash-89 <avinashmamidi89@gmail.com>
2024-05-04 03:01:58 +0530

    home page updated

--- ui-features/src/components/ReactApp/ReactApp.js
+++ ui-features/src/components/ReactApp/ReactApp.js
@@ -1,22 +1,17 @@
-import logo from "../../logo.svg";
+import useRedirectTo from "../../hooks/useRedirectTo";
 import "./ReactApp.css";
 
 function ReactApp() {
+  const { redirectTo } = useRedirectTo();
+  const handleRedirection = (path) => {
+    redirectTo(path);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="header">Na Projects</p>
+        <a onClick={() => handleRedirection("/xoxo")}>XOXO</a>
+        <a onClick={() => handleRedirection("/otpValidator")}>Otp Validator</a>
       </header>
     </div>
   );
